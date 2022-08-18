8a179cf267e631f6a2e0baab738cc8cde185337b
Diego Pontes <54283429+diegout@users.noreply.github.com>
2022-08-17 22:38:18 -0300

    Update and rename programacaoImperativa to checkPointCodigo.js

---
+++ checkPointCodigo.js
@@ -0,0 +1,12 @@
+pipoca = 10;
+macarrao - 8;
+carne = 15;
+feijao - 12;
+brigadeiro = 8;
+
+switch (pipoca) {
+     case 10:
+        console.log("prato bom, bom apetite");
+    case (> 20):
+        
+}
