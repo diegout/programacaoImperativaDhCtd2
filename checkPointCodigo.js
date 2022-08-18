bf3ab6854b455c99c36a2e9f06f30c0b172075cd
Diego Pontes <54283429+diegout@users.noreply.github.com>
2022-08-17 22:39:24 -0300

    Update checkPointCodigo.js

--- checkPointCodigo.js
+++ checkPointCodigo.js
@@ -1,3 +1,6 @@
+
+\\"menu microondas superspeed nhac options"
+
 pipoca = 10;
 macarrao - 8;
 carne = 15;
@@ -8,5 +11,3 @@ switch (pipoca) {
      case 10:
         console.log("prato bom, bom apetite");
     case (> 20):
-        
-}
